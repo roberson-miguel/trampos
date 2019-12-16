60ba46ce72db766a758012d6e083f9ba81c5754f
Roberson Miguel <roberson.miguel@biosbug.com.br>
2019-12-16 16:01:09 -0300

    Criado partial job and company

---
+++ spec/features/company/view_company_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+feature 'View companies' do
+  scenario "must de see a company" do
+
+    Company.create!(name: 'Campus Code', cnpj: '05.372.840/0001-07', address:'Alameda Santos, 011', description: 'Consultoria e Treinamentos em tecnologia, especializada em Ruby on Rails' )
+
+    visit root_path
+    click_link "Empresas"
+    click_link "Campus Code"
+   
+    expect(page).to have_content('Campus Code')
+    expect(page).to have_content('05.372.840/0001-07')
+    expect(page).to have_content('Alameda Santos, 011')
+    expect(page).to have_content('Consultoria e Treinamentos em tecnologia, especializada em Ruby on Rails')
+    expect(page).to have_content('Empresa criada com sucesso.')
+
+  end
+end
