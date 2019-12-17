158df70244428e0efbf3904625c0f372d9e299a2
Roberson Miguel <roberson.miguel@biosbug.com.br>
2019-12-17 08:40:46 -0300

    Validações em Joblevel

--- spec/features/joblevel/register_joblevel_spec.rb
+++ spec/features/joblevel/register_joblevel_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-feature 'Create new Job Level' do
-  scenario "I can create a new job level" do
+feature 'Create new Jobleevel' do
+  scenario "I can create a new joblevel" do
 
     visit root_path
     click_link "Nivel Vaga"
@@ -13,5 +13,49 @@ feature 'Create new Job Level' do
     expect(page).to have_content('Junior')
     expect(page).to have_content('Nivel da vaga criado com sucesso')
 
+  end
+
+  scenario "I can not create a new Joblevel empty" do
+
+    visit root_path
+    click_link "Nivel Vaga"
+    click_link "Novo nivel vaga"
+
+    fill_in 'Nome', with: ' '
+    click_button 'Enviar'
+
+    expect(page).to have_content('...deve ser informado')
+    expect(page).to have_content('...deve ter tamanho minimo de 2 caracter')
+   
+  end
+
+  scenario "I can not create a new joblevel already in use" do
+
+    Joblevel.create(name:'Junior')
+
+    visit root_path
+    click_link "Nivel Vaga"
+    click_link "Novo nivel vaga"
+
+    fill_in 'Nome', with: 'Junior'
+    click_button 'Enviar'
+
+    expect(page).to have_content('...já está em uso')
+   
+  end
+
+  scenario "I can not create a new joblevel without mininum caracter" do
+
+    Joblevel.create(name:'Junior')
+
+    visit root_path
+    click_link "Nivel Vaga"
+    click_link "Novo nivel vaga"
+
+    fill_in 'Nome', with: 'v'
+    click_button 'Enviar'
+
+    expect(page).to have_content('...deve ter tamanho minimo de 2 caracter')
+   
   end
 end
