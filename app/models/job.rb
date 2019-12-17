7ec88f6837af98fa36788144c9d759a3d90f2070
Roberson Miguel <roberson.miguel@biosbug.com.br>
2019-12-17 07:20:55 -0300

    refatorando o enum{salary_range} em Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
 
-    enum salary_range: [:"1000_até_3000", :"3001_até_5000", :"5001_até_8000" ] 
+    enum salary_range: { "1000_até_3000": 0, "3001_até_5000": 15, "5001_até_8000": 30}
 
     def set_default_salary_range
         self.salary_range ||= :"1000_até_3000"
