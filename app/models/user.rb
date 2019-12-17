3d5034283091e77f711e469b9f4effb4e7a0e57e
Roberson Miguel <roberson.miguel@biosbug.com.br>
2019-12-17 18:04:03 -0300

    Views Devise, add role User, error defaul não altera para headhunter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:candidate, :headhunter]
+
+  #after_initialize :set_default_role, :if => :new_record?
+
+  #def set_default_role
+  #  self.role ||= :candidate
+  #end
+
 end
