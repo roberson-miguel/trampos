4af144401dfd09bf003e0f4e189ea86d0d58652e
Roberson Miguel <roberson.miguel@biosbug.com.br>
2019-12-18 07:57:27 -0300

    resolvendo registro de role em user e mensagem de sucesso

--- spec/features/register_as_role_a_user_spec.rb
+++ spec/features/register_as_role_a_user_spec.rb
@@ -3,20 +3,18 @@ require "rails_helper"
 feature 'Register as role a user' do
   scenario "I can create a user as headhunter" do
     
-    user = User.create!(email:'elaine@gmail.com', password:123123, role:'candidate')
-
     visit root_path
     click_link "Sair"
     click_link "Registrar"
 
-    fill_in 'Email', with: 'elaine@gmail.com'
+    fill_in 'Email', with: 'elaine@gmail'
     select 'Headhunter', from: 'Função'
     fill_in 'Password', with:123123
     fill_in 'Password confirmation', with:123123
 
     click_button 'Sign up'
     
-    #expect(page).to have_content('Your account has been updated successfully.')
+    expect(page).to have_content('Welcome! You have signed up successfully.')
 
   end
 end
