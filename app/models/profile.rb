2edad0710e39bb4974e51de9f8b850dce3fff9ad
Roberson Miguel <roberson.miguel@biosbug.com.br>
2019-12-18 11:30:36 -0300

    enum profile criado

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,19 @@ class Profile < ApplicationRecord
   belongs_to :skill
 
   #validates :name, presence: {message: '...deve ser informado'}
+
+    enum languages: { "inglês(basico)": 0, "inglês(intermediario)": 15, "inglês(avançado)": 30}
+    def set_default_languages
+        self.languages ||= :"inglês(basico)"
+    end
+
+    enum education_level: { "fundamental": 0, "técnico": 15, "graduado": 30}
+    def set_default_education_level
+        self.education_level ||= :"graduado"
+    end
+
+    enum education_status: { "cursando": 0, "trancado": 15, "concluido": 30}
+    def set_default_education_status
+        self.education_status ||= :"concluido"
+    end
 end
